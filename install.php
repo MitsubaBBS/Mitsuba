b5a7c7ca91d1a8a99b69099ef18a84ceabc3db19
onexxen <onexxen@gmail.com>
2013-02-20 22:53:48 +0000

    Fixed install.php

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 if (file_exists("./config.php"))
 {
-//die("Y U R TRYIN TO HACK THIS WONDERFUL SCRIPT?");
+die("Y U R TRYIN TO HACK THIS WONDERFUL SCRIPT?");
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
