d42eb686f8ff2464a00c1d60510219c4fe376386
Desuneko <onexxen@gmail.com>
2013-04-27 23:52:50 +0200

    Fixed banned.php

--- banned.php
+++ banned.php
@@ -6,6 +6,7 @@ header("Location: ./install.php");
 
 include("config.php");
 include("inc/common.php");
+$conn = new mysqli($db_host, $db_username, $db_password, $db_database);
 banMessage($conn, $board);
 ?>
 <h1>NOT BANNED</h1>
