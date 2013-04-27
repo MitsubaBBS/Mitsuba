366d0f311c7540933e70e8286863d45c52b16094
Desuneko <onexxen@gmail.com>
2013-04-27 23:44:48 +0200

    Moved parse_time() from admin.bans.php to common.php

--- inc/common.php
+++ inc/common.php
@@ -529,4 +529,48 @@ function pruneOld($conn, $board)
 		}
 	}
 }
+
+function parse_time($str) {
+	if (empty($str))
+		return false;
+
+	if (($time = @strtotime($str)) !== false)
+		return $time;
+
+	if (!preg_match('/^((\d+)\s?ye?a?r?s?)?\s?+((\d+)\s?mon?t?h?s?)?\s?+((\d+)\s?we?e?k?s?)?\s?+((\d+)\s?da?y?s?)?((\d+)\s?ho?u?r?s?)?\s?+((\d+)\s?mi?n?u?t?e?s?)?\s?+((\d+)\s?se?c?o?n?d?s?)?$/', $str, $matches))
+		return false;
+
+	$expire = 0;
+
+	if (isset($matches[2])) {
+		// Years
+		$expire += $matches[2]*60*60*24*365;
+	}
+	if (isset($matches[4])) {
+		// Months
+		$expire += $matches[4]*60*60*24*30;
+	}
+	if (isset($matches[6])) {
+		// Weeks
+		$expire += $matches[6]*60*60*24*7;
+	}
+	if (isset($matches[8])) {
+		// Days
+		$expire += $matches[8]*60*60*24;
+	}
+	if (isset($matches[10])) {
+		// Hours
+		$expire += $matches[10]*60*60;
+	}
+	if (isset($matches[12])) {
+		// Minutes
+		$expire += $matches[12]*60;
+	}
+	if (isset($matches[14])) {
+		// Seconds
+		$expire += $matches[14];
+	}
+
+	return time() + $expire;
+}
 ?>
