98a760e077df8ee5beacece5b6ab06dd77f674f6
Desuneko <onexxen@gmail.com>
2013-08-10 12:03:53 +0200

    ui: fixed login form

--- inc/mod/main.inc.php
+++ inc/mod/main.inc.php
@@ -24,7 +24,25 @@ if ((isset($_SESSION['logged'])) && ($_SESSION['logged']==1))
 <?php $mitsuba->admin->ui->startSection($lang['mod/log_in']); ?>
 
 <form action="?/login" method="POST">
-<center><?php echo $lang['mod/username']; ?>: <input type="text" name="username" /> | <?php echo $lang['mod/password']; ?>: <input type="password" name="password" /> <input type="submit" value="<?php echo $lang['mod/log_in']; ?>" /></center>
+<table class="postForm">
+	<tbody>
+		<tr>
+			<td style="text-align: center; width: 65px;"><?php echo $lang['mod/username']; ?></td>
+			<td><input type="text" name="username" style="width: 145px; text-align: center;"></td>
+		</tr>
+
+		<tr>
+			<td style="text-align: center; width: 65px;"><?php echo $lang['mod/password']; ?></td>
+			<td><input type="password" name="password" style="width: 145px; text-align: center;"></td>
+		</tr>
+
+		<tr>
+			<td colspan="2" style="padding: 5px 0; border: none; background: none; text-align: center; font-weight: normal; padding-bottom: 20px;">
+				<input type="submit" value="<?php echo $lang['mod/log_in']; ?>" style="margin: 0px;">
+			</td>
+		</tr>
+	</tbody>
+</table>
 </form>
 <?php $mitsuba->admin->ui->endSection(); ?>
 		<?php
