8cce629d7608d94c34e2fe80034eaae93ffbaf70
Desuneko <onexxen@gmail.com>
2013-09-27 18:35:48 +0200

    rebuild: fixed Insufficient permissions error

--- inc/mod/static.inc.php
+++ inc/mod/static.inc.php
@@ -3,7 +3,7 @@ if (!defined("IN_MOD"))
 {
 	die("Nah, I won't serve that file to you.");
 }
-$mitsuba->admin->reqPermission("rebuild");
+$mitsuba->admin->reqPermission("config.rebuild");
 $mitsuba->admin->ui->checkToken($_POST['token']);
 		if ((!empty($_POST['frontpage'])) && ($_POST['frontpage']==1))
 		{
