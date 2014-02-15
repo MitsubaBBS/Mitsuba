dd357ef12a7729246f019c0af141a38d6d084992
William Compton <nieprzejednany@gmail.com>
2014-02-15 21:27:47 +0100

    small captcha fix

--- captcha.php
+++ captcha.php
@@ -16,7 +16,7 @@ if (isset($_GET['t']))
 	}
 	$captcha->CreateImage();
 } elseif (isset($_GET['i'])) {
-	echo "<a style='position: absolute; left: 0; top: 0;' href='?i=".time()."'><img style='width: 100%;' src='?t=".time()."' alt='CAPTCHA'/></a>";
+	echo "<a style='position: absolute; left: 0; top: 0;' href='?i=".time()."'><img style='width: 100%; height: 70px;' src='?t=".time()."' alt='CAPTCHA'/></a>";
 } else {
 	echo "<a style='display: block; padding-top: 10px; text-align: center; text-decoration: none; color: #FFF; font-size: 30px; text-shadow: -1px 0px #000, 0px 1px #000, 1px 0px #000, 0px -1px #000' href='?i=".time()."'>Click here</a>";
 }
